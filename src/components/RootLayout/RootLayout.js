2cdf9a8535eb6c46bf30bfe2a662fab5668a16c5
Pieniu1511 <pieniu1511@gmail.com>
2023-05-12 19:18:45 +0200

    changed Footer directory

--- src/components/RootLayout/RootLayout.js
+++ src/components/RootLayout/RootLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Footer from '../Footer/Footer'
+import Footer from '../footer/Footer'
 import Header from '../header/Header'
 
 function RootLayout() {
